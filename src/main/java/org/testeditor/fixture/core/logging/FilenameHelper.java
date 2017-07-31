136fd4556ed27e4edb636f67d414f3fe5de39fe8
Ersin Bayraktar <ersin.koray.bayraktar@signal-iduna.de>
2017-07-31 11:14:55 +0200

    TE-503 Creating screenshots

---
+++ src/main/java/org/testeditor/fixture/core/logging/FilenameHelper.java
@@ -0,0 +1,52 @@
+package org.testeditor.fixture.core.logging;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FilenameHelper {
+
+    private static final int FILENAME_MAXLEN = 128;
+
+    /**
+     * Constructs a file name, based on the basic filenameBase provided. The
+     * final filename is constructed using the testcase a hash of the fixture
+     * itself and a shortened timestamp.
+     * 
+     * @param pathName
+     *            the path in which the file is stored
+     * @param testcase
+     *            name of the test case which is executed
+     * @param filenameBase
+     *            user definable part of the final filename
+     * @param type
+     *            File type like "html", "png" or equivalent, depends which type
+     *            should be created
+     * @return the constructed filename with the above attributes
+     */
+    public String constructFilename(String pathName, String testcase, String filenameBase, String type) {
+        String additionalGraphicType = "." + type;
+        String escapedBaseName = filenameBase.replaceAll("[^a-zA-Z0-9.-]", "_").replaceAll("_+", "_")
+                .replaceAll("_+\\.", ".").replaceAll("\\._+", ".");
+        String timeStr = new SimpleDateFormat("HHmmss.SSS").format(new Date());
+        String dateStr = new SimpleDateFormat("YYYYMMdd").format(new Date());
+        StringBuffer finalFilenameBuffer = new StringBuffer();
+        int lenOfFixedElements = timeStr.length() + additionalGraphicType.length() + 1/* hyphen */;
+        finalFilenameBuffer //
+                .append(pathName) //
+                .append('/').append(reduceToMaxLen(testcase, FILENAME_MAXLEN))//
+                .append('/').append(dateStr) //
+                .append('/').append(timeStr).append('-') //
+                .append(reduceToMaxLen(escapedBaseName, FILENAME_MAXLEN - lenOfFixedElements))//
+                .append(additionalGraphicType);
+        return finalFilenameBuffer.toString();
+    }
+
+    private String reduceToMaxLen(String base, int maxLen) {
+        if (base.length() < maxLen) {
+            return base;
+        } else {
+            return base.substring(0, maxLen);
+        }
+    }
+
+}
